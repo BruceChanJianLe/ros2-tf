f92ee35267cf5ed44a8d49e689a49d544e378192
chanjl <chanjl@i2r.a-star.edu.sg>
2022-06-27 22:52:36 +0800

    add all for next dev

---
+++ launch/static_transform_publisher.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    static_broadcaster_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='map_to_odom',
+        arguments=['1', '0', '0', '0', '0', '0', 'map', 'odom']
+    )
+
+    # Define launch description
+    ld = LaunchDescription()
+    ld.add_action(static_broadcaster_node)
+
+    # Return launch description
+    return ld
